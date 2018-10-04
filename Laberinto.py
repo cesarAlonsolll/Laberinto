8dda968131a1a73943d09d26db7bf0be097794e2
cesarAlonsolll <43044721+cesarAlonsolll@users.noreply.github.com>
2018-10-04 15:38:50 -0500

    comprobaciones basicas del laberinto

--- Laberinto.py
+++ Laberinto.py
@@ -4,8 +4,22 @@ def cargarLaberinto(nombre):
     
     lector = open(nombre,"r")
     laberinto = lector.read().replace(' ','').replace(',','').split('\n')
+    laberinto = [tuple(map(int, tupla)) for tupla in laberinto]
     return tuple([tuple(linea) for linea in laberinto])
+
+def comprobarValidezLaberinto(laberinto):
+    if (len(laberinto)-2)>3 and len(laberinto[0])>3:
+        return not(False in [len(laberinto[0])==len(linea) for linea in laberinto][:-2])
+    return False
+
+def comprobarValidezCoordenadas(laberinto):
+    if laberinto[-1][0]<(len(laberinto)-2) and laberinto[-2][0]<(len(laberinto)-2):
+        if laberinto[-1][1]<len(laberinto[0]) and laberinto[-2][1]<len(laberinto[0]):
+            return True
+    return False
+
+
 
 laberinto = cargarLaberinto(nombreArchivo)
 
-print laberinto
+print comprobarValidezCoordenadas(laberinto)
